39ae2d1755f13fd334998f8defe24d87bab08403
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-06-15 15:15:48 -0500

    added archiving, changed plotting

--- main_API_pull_functions.py
+++ main_API_pull_functions.py
@@ -7,10 +7,11 @@ Created on Tue Mar 29 11:35:57 2022
 """
 import requests
 import pandas as pd
-from datetime import date,timedelta
+from datetime import date,timedelta,datetime
 import numpy as np
-from plotnine import ggplot,aes,geom_line,geom_point,theme_bw,labs,xlab,ylab,scale_colour_manual
 import os
+import seaborn as sns
+import shutil 
 
 def identify_last_day_updates(apiToken,apiURL): #this function identifies each unique MRN that either has an updated response action or a created record action in the last 24 hours
     last_24_hours=str(date.today()-timedelta(days=1)) + " 00:00"
@@ -45,6 +46,7 @@ def extract_data(mrn,apiToken,apiURL): #this function extracts all exisiting pcl
         'forms[2]': 'phq9',
         'forms[3]': 'ptci',
         'forms[4]': 'pcl_past_week',
+        'forms[5]': 'participant_validation',
         'records[0]': mrn,
         'events[0]': 'baseline_survey_arm_1',
         'events[1]': 'monday_week_1_arm_1',
@@ -99,10 +101,18 @@ def extract_complete_record_set(mrns,apiToken,apiURL):
     return complete_records
 
 def clean_records(df):
-    relevant_records=df[['mrn','redcap_event_name','cohort','pcl5_score','pcl5_score_pastmonth','phq9_score','phq9s_score','ptci_score']]
+    relevant_records=df[['mrn','first_name','last_name','date','redcap_event_name','cohort','pcl5_score','pcl5_score_pastmonth','phq9_score','phq9s_score','ptci_score']]
     relevant_records=relevant_records.assign(pcl5_score_all=np.where(relevant_records['pcl5_score']=="",relevant_records['pcl5_score_pastmonth'],relevant_records['pcl5_score']))
     relevant_records=relevant_records.loc[relevant_records['redcap_event_name'].str.contains('arm_1')]
     relevant_records=relevant_records.replace("",np.NaN)
+    relevant_records['pcl5_score'] = relevant_records['pcl5_score'].astype(float)
+    relevant_records['pcl5_score_pastmonth'] = relevant_records['pcl5_score_pastmonth'].astype(float)
+    relevant_records['pcl5_score_all'] = relevant_records['pcl5_score_all'].astype(float)
+    relevant_records['phq9_score'] = relevant_records['phq9_score'].astype(float)
+    relevant_records['phq9s_score'] = relevant_records['phq9s_score'].astype(float)
+    relevant_records['ptci_score'] = relevant_records['ptci_score'].astype(float)
+    monovalue_records=relevant_records['mrn'].value_counts()[relevant_records['mrn'].value_counts()==1].index.tolist()
+    relevant_records=relevant_records.loc[~relevant_records['mrn'].isin(monovalue_records)]
     return(relevant_records)
 
 def create_plot_obj(mrn,df):
@@ -140,126 +150,61 @@ def create_plot_obj(mrn,df):
                 "6Mon",
                 "12Mon"]
     tmp['redcap_event_name']=np.select(condlist,choicelist,"null")
-    #pcl
-    pcl_tmp=tmp.loc[tmp['pcl5_score_all'] == tmp['pcl5_score_all']]
-    time_cat=pd.Categorical(pcl_tmp['redcap_event_name'], categories=["Baseline",
-                "Mon w1",
-                "Tue w1",
-                "Wed w1",
-                "Thur w1",
-                "Fri w1",
-                "Mon w2",
-                "Tue w2",
-                "Wed w2",
-                "Thur w2",
-                "Post",
-                "1Mon",
-                "3Mon",
-                "6Mon",
-                "12Mon"])
-    pcl_tmp=pcl_tmp.assign(time_cat=time_cat)
+    dat=tmp.drop(['first_name','last_name','date','pcl5_score_all'],axis=1)
     
-    gPCL=(ggplot(pcl_tmp,aes(x="time_cat",y="pcl5_score_all",group="mrn",colour="mrn")) + 
-     geom_point()+
-     geom_line()+
-     theme_bw() +
-     ylab("PCL-5 Score") +
-     xlab("Measurement Time Point") +
-     scale_colour_manual(values='#1D6F42') +
-     labs(colour="MRN"))
-    #phq
-    phq_tmp=tmp.loc[tmp['phq9_score'] == tmp['phq9_score']]
-    time_cat=pd.Categorical(phq_tmp['redcap_event_name'], categories=["Baseline",
-                "Mon w1",
-                "Tue w1",
-                "Wed w1",
-                "Thur w1",
-                "Fri w1",
-                "Mon w2",
-                "Tue w2",
-                "Wed w2",
-                "Thur w2",
-                "Post",
-                "1Mon",
-                "3Mon",
-                "6Mon",
-                "12Mon"])
-    phq_tmp=phq_tmp.assign(time_cat=time_cat)
+    tmp=tmp.drop(["pcl5_score",'pcl5_score_pastmonth','cohort','first_name','last_name','date'],axis=1)
     
-    gPHQ=(ggplot(phq_tmp,aes(x="time_cat",y="phq9_score",group="mrn",colour="mrn")) + 
-     geom_point()+
-     geom_line()+
-     theme_bw() +
-     ylab("PHQ-9 Score") +
-     xlab("Measurement Time Point") +
-     scale_colour_manual(values='#1D6F42') +
-     labs(colour="MRN"))
-    #phqs
-    phqs_tmp=tmp.loc[tmp['phq9s_score'] == tmp['phq9s_score']]
-    time_cat=pd.Categorical(phqs_tmp['redcap_event_name'], categories=["Baseline",
-                "Mon w1",
-                "Tue w1",
-                "Wed w1",
-                "Thur w1",
-                "Fri w1",
-                "Mon w2",
-                "Tue w2",
-                "Wed w2",
-                "Thur w2",
-                "Post",
-                "1Mon",
-                "3Mon",
-                "6Mon",
-                "12Mon"])
-    phqs_tmp=phqs_tmp.assign(time_cat=time_cat)
+    tmp=pd.melt(tmp,id_vars=['mrn','redcap_event_name'],value_vars=['phq9_score','phq9s_score', 'ptci_score', 'pcl5_score_all'])
+#    tmp=tmp.loc[tmp['value'] == tmp['value']]
+    condlist=[tmp['variable'].str.contains("pcl5_score_all"),
+              tmp['variable'].str.contains("ptci_score"),
+              tmp['variable'].str.contains("phq9_score"),
+              tmp['variable'].str.contains("phq9s_score")]
+    choicelist=["PCL-5",
+                "PTCI",
+                "PHQ-9",
+                "PHQ-9s"]
+    tmp['variable']=np.select(condlist,choicelist,"null")
     
-    gPHQs=(ggplot(phq_tmp,aes(x="time_cat",y="phq9s_score",group="mrn",colour="mrn")) + 
-     geom_point()+
-     geom_line()+
-     theme_bw() +
-     ylab("PHQ-9s Score") +
-     xlab("Measurement Time Point") +
-     scale_colour_manual(values='#1D6F42') +
-     labs(colour="MRN"))
-    #ptci
-    ptci_tmp=tmp.loc[tmp['ptci_score'] == tmp['ptci_score']]
-    time_cat=pd.Categorical(ptci_tmp['redcap_event_name'], categories=["Baseline",
-                "Mon w1",
-                "Tue w1",
-                "Wed w1",
-                "Thur w1",
-                "Fri w1",
-                "Mon w2",
-                "Tue w2",
-                "Wed w2",
-                "Thur w2",
-                "Post",
-                "1Mon",
-                "3Mon",
-                "6Mon",
-                "12Mon"])
-    ptci_tmp=ptci_tmp.assign(time_cat=time_cat)
+    sns.set(rc={'figure.figsize':(28,20),'figure.dpi':(300)})
+    g=sns.FacetGrid(tmp,col="variable",sharey=False,col_wrap=2)
+    g.axes[0].set_ylim(0,27)
+    g.axes[1].set_ylim(0,4)
+    g.axes[2].set_ylim(0,250)
+    g.axes[3].set_ylim(0,80)
+    g.axes[0].set_xticklabels(g.axes[0].get_xticklabels(), rotation=45)
+    g.axes[1].set_xticklabels(g.axes[1].get_xticklabels(), rotation=45)
+    g.axes[2].set_xticklabels(g.axes[2].get_xticklabels(), rotation=45)
+    g.axes[3].set_xticklabels(g.axes[3].get_xticklabels(), rotation=45)
+    p=g.map_dataframe(sns.pointplot,x="redcap_event_name",y="value")
+    p2 = g.map_dataframe(sns.lineplot,x="redcap_event_name",y="value",lw=2.5)
+    p.set_titles('{col_name}')
+    return p, dat
     
-    gPTCI=(ggplot(ptci_tmp,aes(x="time_cat",y="ptci_score",group="mrn",colour="mrn")) + 
-     geom_point()+
-     geom_line()+
-     theme_bw() +
-     ylab("PTCI Score") +
-     xlab("Measurement Time Point") +
-     scale_colour_manual(values='#1D6F42') +
-     labs(colour="MRN"))
-    return gPCL, gPHQ, gPHQs, gPTCI, tmp
-
-
 def lookup_cohort(mrn,df):
     cohort=df.loc[df['mrn']==mrn]['cohort'].dropna().reset_index(drop=True)[0]
     return cohort
 
+def lookup_cohort_startdate(cohort_num,df):
+    tmp=df.loc[(df['cohort']==cohort_num)]['mrn']
+    tmp=df.loc[df['mrn'].isin(tmp)]
+    tmp=tmp.loc[(tmp['redcap_event_name'] == "monday_week_1_arm_1")]['date']
+    tmp=pd.to_datetime(tmp,format="%Y-%m-%d")
+    sd=min(tmp).month_name() + " " + str(min(tmp).date().day)
+    year=str(min(tmp).year)
+    return sd, year
+
+def lookup_initials(mrn,df):
+    tmp=df.loc[df['mrn'] == mrn]
+    tmp=tmp[['first_name','last_name']]
+    tmp=tmp.loc[(tmp['first_name'] == tmp['first_name']) & (tmp['last_name'] == tmp['last_name'])]
+    initials=tmp.iloc[0,0][0] + tmp.iloc[0,1][0]
+    return(initials)
 #lookup outdir or create one if none exists
-def get_outpath(root_out,mrn,cohort_num):
+def get_outpath(root_out,mrn,cohort_num,sd,year,initials):
     if not root_out.endswith("/"):
         root_out=root_out + "/"
-    outpath=root_out + "COHORT" + "_" + cohort_num +"/" + mrn + "/"
+    outpath=root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/"
     if not os.path.isdir(outpath):
         os.makedirs(outpath)
     return outpath
@@ -273,6 +218,21 @@ def preverify_SSL():
         print(requests.exceptions.SSLError)
         exit()
 
+def archive_previous(root_out,mrn,cohort_num,sd,year,initials):
+    timestamp="archival_timestamp_" + datetime.now().strftime("%m.%d.%Y_%H.%M")
+    if not os.path.isdir(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/"):
+        os.makedirs(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/")
+    os.makedirs(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" +timestamp)
+    png_out=root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/" + initials + "_" + mrn + ".png"
+    csv_out=root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/" + initials + "_" + mrn + "_summary.csv"
+    if os.path.isfile(png_out):
+        shutil.move(png_out, root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp+ "/" + initials + "_" + mrn + ".png")
+    if os.path.isfile(csv_out):
+        shutil.move(csv_out, root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp+ "/" + initials + "_" + mrn + "_summary.csv")
+    if len(os.listdir(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp + "/")) != 0 :
+        shutil.make_archive(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp + ".zip",'zip',root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp + "/")
+        shutil.rmtree(root_out + year + "/" + sd + "/" + "COHORT" + "_" + cohort_num +"/" + initials + "_" + mrn + "/archive/" + initials + "_" + mrn + "_" + timestamp + "/")
+    
 def main(apiToken,apiURL,root_out):
     preverify_SSL()
     mrns=identify_last_day_updates(apiToken,apiURL)
@@ -282,12 +242,10 @@ def main(apiToken,apiURL,root_out):
     for mrn in mrns:
         print(mrn)
         cohort_number=lookup_cohort(mrn=mrn,df=records)
-        outpath=get_outpath(root_out=root_out,mrn=mrn,cohort_num=cohort_number)
-        gPCL, gPHQ, gPHQs, gPTCI, dat=create_plot_obj(mrn=mrn,df=records)
-        dat=dat.drop('pcl5_score_all',axis=1)
-        dat.to_csv(outpath + mrn + "_summary.csv")
-        if dat.shape[0] > 1 :
-            gPCL.save(filename=outpath + mrn + "_PCL-5.png",dpi=300,height=4.9,width=7)
-            gPHQ.save(filename=outpath + mrn + "_PHQ-9.png",dpi=300,height=4.9,width=7)
-            gPHQs.save(filename=outpath + mrn + "_PHQ-9s.png",dpi=300,height=4.9,width=7)
-            gPTCI.save(filename=outpath + mrn + "_PTCI.png",dpi=300,height=4.9,width=7)
+        start,year=lookup_cohort_startdate(cohort_num=cohort_number,df=records)
+        initials=lookup_initials(mrn=mrn,df=records)
+        outpath=get_outpath(root_out=root_out,mrn=mrn,cohort_num=cohort_number,sd=start,year=year,initials=initials)
+        archive_previous(root_out=root_out,mrn=mrn,cohort_num=cohort_number,sd=start,year=year,initials=initials)
+        p,dat=create_plot_obj(mrn=mrn,df=records)
+        dat.to_csv(outpath + initials + "_" + mrn + "_summary.csv")
+        p.savefig(fname=outpath + initials + "_" + mrn + ".png")
